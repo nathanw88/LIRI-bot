555d200cc956efa45cb7da2eabd4fde43c85cf26
Nate <nathanw88@gmail.com>
2018-12-09 16:40:17 -0600

    added comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# LIRI-bot
+# LiriBot
+
+takes in arguments to search spotify, bands in town, or OMDB. can use input from command line to start the search if none is given it will prompt user for needed info
+
+## Getting Started
+
+after cloning this down you will need to enter the command npm i in the command line while in the directiory to get all the required node packages, you will need a .env file with the requierd keys. in this format
+
+```
+# Spotify API keys
+
+SPOTIFY_ID =  Your-spotify-key
+SPOTIFY_SECRET =	Your-spotify-secret
+
+# Bands in town API keys
+
+bands_API = Your-bands-key
+
+# OMDB API keys
+
+OMDB_API = Your-OMDB-key
+```
+
+## Screenshots
+
+![starting with no arguments!](./screenshots/start-no-arguments.png?raw=true "Starting without an argument")
+
+![concert search](./screenshots/concert-search.png?raw=true "Upon selecting concert-this")
+
+![concert results](./screenshots/concert-results.png?raw=true "The results returned from concert this")
+
+![movie & and spotify results](./screenshots/capture.png?raw=true "Results from movie & spotify")
+
+![random.txt results](./screenshots/do-what-it-says?raw=true "Results from arguments in random.txt")
+
+![command line arguments](./screenshots/comand-line-arguments.png?raw=true "Passing in command line arguments")
--- liri.js
+++ liri.js
@@ -1,19 +1,21 @@
+// setting up all the requires
 require("dotenv").config();
 var keys = require("./keys");
 var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var inquirer = require("inquirer");
 var axios = require("axios");
-var rp = require("request-promise");
-var request = require("request");
-
+var fs = require("fs");
+// setting api keys to variables to be used
 var spotify = new Spotify(keys.spotify);
 var bands = keys.bands;
 var OMDB = keys.OMDB;
-
+// two variables to be used for user input
 var userChoice;
 var userSearch;
 
+// function to be called to grab what to search for
+// parameter messages is passed depending on what api we are going to call
 function toSearch(message) {
   inquirer
     .prompt([
@@ -24,20 +26,22 @@ function toSearch(message) {
       }
     ])
     .then(function(response) {
-      console.log(message);
-      console.log(response.toSearch);
       if (message === "Song to search!" && response.toSearch === "") {
         userSearch = "The Sign";
         checkingToDo();
       } else if (message === "Movie to search!" && response.toSearch === "") {
         userSearch = "Mr. Nobody";
         checkingToDo();
+      } else if (response.toSearch.toLowerCase === "back") {
+        userChoice = undefined;
+        toDo();
       } else {
         userSearch = response.toSearch;
         checkingToDo();
       }
     });
 }
+// function that takes user input on what api to call, to quit, or to read commands from random.txt
 function toDo() {
   inquirer
     .prompt([
@@ -60,11 +64,12 @@ function toDo() {
     });
 }
 
+// first function to be called checks if there were any command line arguments passed in
 function userInput() {
   debugger;
   userChoice = process.argv[2];
   userSearch = process.argv.splice(3).join(" ");
-  console.log(userSearch);
+
   if (userChoice === undefined) {
     toDo();
   } else {
@@ -72,6 +77,7 @@ function userInput() {
   }
 }
 
+//looks to see what API user choose and if they have decided what search parameter to pass it
 function checkingToDo() {
   switch (userChoice) {
     case "concert-this":
@@ -99,6 +105,16 @@ function checkingToDo() {
       break;
 
     case "do-what-it-says":
+      fs.readFile("./random.txt", "utf8", function(err, data) {
+        var arr = data.split(",");
+        userChoice = arr[0];
+        userSearch = arr[1];
+        checkingToDo();
+      });
+
+      break;
+
+    case "quit":
       break;
 
     default:
@@ -108,6 +124,7 @@ function checkingToDo() {
 }
 userInput();
 
+// function for searching OMDB
 function movieSearch() {
   axios
     .get(
@@ -124,9 +141,27 @@ function movieSearch() {
       console.log(`Language: ${response.data.Language}`);
       console.log(`Plot: ${response.data.Plot}`);
       console.log(`Actors: ${response.data.Actors}`);
+      fs.appendFile(
+        "log.txt",
+        JSON.stringify(`${userChoice} Title: ${response.data.Title}
+        Year: ${response.data.Year}
+        IMDB Rating: ${response.data.imdbRating}
+        ${response.data.Ratings[1].Source}: ${response.data.Ratings[1].Value}
+        Country: ${response.data.Country}
+        Language: ${response.data.Language}
+        Plot: ${response.data.Plot}
+        Actors: ${response.data.Actors}`),
+        err => {
+          if (err) throw err;
+        }
+      );
+      userSearch = "";
+      userChoice = undefined;
+      toDo();
     });
 }
 
+//Function to search bands in town
 function bandSearch() {
   axios
     .get(
@@ -135,28 +170,79 @@ function bandSearch() {
       }`
     )
     .then(function(response) {
-      console.log(response.data);
       for (var i = 0; i < response.data.length; i++) {
         console.log(`Venue: ${response.data[i].venue.name}
           Location: ${response.data[i].venue.city}, ${
           response.data[i].venue.country
         }
           Time: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}`);
+        fs.appendFile(
+          "log.txt",
+          JSON.stringify(`${userChoice} ${userSearch} Venue: ${
+            response.data[i].venue.name
+          }
+        Location: ${response.data[i].venue.city}, ${
+            response.data[i].venue.country
+          }
+        Time: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}`),
+          err => {
+            if (err) throw err;
+          }
+        );
       }
+      userSearch = "";
+      userChoice = undefined;
+      toDo();
     });
 }
+
+//function to search spotify
 function songSearch() {
   spotify
     .search({ type: "track", query: `${userSearch}` })
     .then(function(response) {
+      var matches = 0;
       for (var i = 0; i < response.tracks.items.length; i++) {
-        console.log(`Artist: ${response.tracks.items[i].artists[0].name}`);
-        console.log(`Title: ${response.tracks.items[i].name}`);
-        console.log(
-          `Link to Song: ${response.tracks.items[i].external_urls.spotify}`
-        );
-        console.log(`Album: ${response.tracks.items[i].album.name}`);
+        if (
+          response.tracks.items[i].name.toLowerCase() ===
+          userSearch.toLowerCase()
+        ) {
+          matches++;
+          console.log(`Artist: ${response.tracks.items[i].artists[0].name}`);
+          console.log(`Title: ${response.tracks.items[i].name}`);
+          console.log(
+            `Link to Song: ${response.tracks.items[i].external_urls.spotify}`
+          );
+          console.log(`Album: ${response.tracks.items[i].album.name}`);
+          fs.appendFile(
+            "log.txt",
+            JSON.stringify(`${userChoice} ${userSearch} Artist: ${
+              response.tracks.items[i].artists[0].name
+            }
+            Title: ${response.tracks.items[i].name}
+            Link to Song: ${
+              response.tracks.items[i].external_urls.spotify
+            } Album: ${response.tracks.items[i].album.name}`),
+            err => {
+              if (err) throw err;
+            }
+          );
+        }
       }
+      if (matches === 0) {
+        for (var i = 0; i < response.tracks.items.length; i++) {
+          matches++;
+          console.log(`Artist: ${response.tracks.items[i].artists[0].name}`);
+          console.log(`Title: ${response.tracks.items[i].name}`);
+          console.log(
+            `Link to Song: ${response.tracks.items[i].external_urls.spotify}`
+          );
+          console.log(`Album: ${response.tracks.items[i].album.name}`);
+        }
+      }
+      userSearch = "";
+      userChoice = undefined;
+      toDo();
     })
     .catch(function(err) {
       console.log(err);
