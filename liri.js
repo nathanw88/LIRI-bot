821fede0008462086f647536098d494d95f6688b
Nate <nathanw88@gmail.com>
2018-12-09 12:43:22 -0600

    Have all data showing to screen

--- liri.js
+++ liri.js
@@ -4,14 +4,16 @@ var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var inquirer = require("inquirer");
 var axios = require("axios");
+var rp = require("request-promise");
+var request = require("request");
 
 var spotify = new Spotify(keys.spotify);
 var bands = keys.bands;
 var OMDB = keys.OMDB;
-console.log(bands.id);
 
 var userChoice;
 var userSearch;
+
 function toSearch(message) {
   inquirer
     .prompt([
@@ -22,8 +24,18 @@ function toSearch(message) {
       }
     ])
     .then(function(response) {
-      userSearch = response.toSearch;
-      checkingToDo();
+      console.log(message);
+      console.log(response.toSearch);
+      if (message === "Song to search!" && response.toSearch === "") {
+        userSearch = "The Sign";
+        checkingToDo();
+      } else if (message === "Movie to search!" && response.toSearch === "") {
+        userSearch = "Mr. Nobody";
+        checkingToDo();
+      } else {
+        userSearch = response.toSearch;
+        checkingToDo();
+      }
     });
 }
 function toDo() {
@@ -81,6 +93,8 @@ function checkingToDo() {
     case "spotify-this-song":
       if (userSearch === "") {
         toSearch("Song to search!");
+      } else {
+        songSearch();
       }
       break;
 
@@ -100,7 +114,16 @@ function movieSearch() {
       `http://www.omdbapi.com/?t=${userSearch}&y=&plot=short&apikey=${OMDB.id}`
     )
     .then(function(response) {
-      console.log(response.data);
+      console.log(`Title: ${response.data.Title}`);
+      console.log(`Year: ${response.data.Year}`);
+      console.log(`IMDB Rating: ${response.data.imdbRating}`);
+      console.log(
+        `${response.data.Ratings[1].Source}: ${response.data.Ratings[1].Value}`
+      );
+      console.log(`Country: ${response.data.Country}`);
+      console.log(`Language: ${response.data.Language}`);
+      console.log(`Plot: ${response.data.Plot}`);
+      console.log(`Actors: ${response.data.Actors}`);
     });
 }
 
@@ -113,5 +136,29 @@ function bandSearch() {
     )
     .then(function(response) {
       console.log(response.data);
+      for (var i = 0; i < response.data.length; i++) {
+        console.log(`Venue: ${response.data[i].venue.name}
+          Location: ${response.data[i].venue.city}, ${
+          response.data[i].venue.country
+        }
+          Time: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}`);
+      }
+    });
+}
+function songSearch() {
+  spotify
+    .search({ type: "track", query: `${userSearch}` })
+    .then(function(response) {
+      for (var i = 0; i < response.tracks.items.length; i++) {
+        console.log(`Artist: ${response.tracks.items[i].artists[0].name}`);
+        console.log(`Title: ${response.tracks.items[i].name}`);
+        console.log(
+          `Link to Song: ${response.tracks.items[i].external_urls.spotify}`
+        );
+        console.log(`Album: ${response.tracks.items[i].album.name}`);
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
